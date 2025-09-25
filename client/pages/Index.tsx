1242cd772b230d5a5f675ee8a0a0286e7690d68c
ermalkomoni <ermalkk14@gmail.com>
2025-09-26 00:09:24 +0200

    adding scroll indicator

--- client/pages/Index.tsx
+++ client/pages/Index.tsx
@@ -112,6 +112,13 @@ export default function Index() {
             </div>
           </div>
         </div>
+        
+        {/* Scroll indicator */}
+        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+          <div className="w-6 h-10 border-2 border-muted-foreground rounded-full flex justify-center">
+            <div className="w-1 h-3 bg-muted-foreground rounded-full mt-2 animate-pulse"></div>
+          </div>
+        </div>
       </section>
 
       {/* Quick About Section */}
